fdf67735e2c87db7cecf9f99703567097a9e20b8
Vansh <vanshmorwani1@gmail.com>
2025-08-26 18:34:19 +0530

    added common packages

---
+++ packages/common/src/types.ts
@@ -0,0 +1,14 @@
+import {email, z} from "zod"
+
+export const CreateUserSchema = z.object({
+    username:z.string().min(4).max(20),
+    email: z.email(),
+    password:z.string().min(8).max(20)
+})
+export const SignInSchema = z.object({
+    username:z.string().min(4).max(20),
+    password:z.string().min(8).max(20)
+})
+export const CreateRoomSchema = z.object({
+    roomName:z.string().min(4).max(20)
+})
